91214438801fb06164d0df7c0c7306f09d8dbdb0
Mohammed Abuteir <moh2015moh21415@gmail.com>
2026-08-02 12:55:51 +0300

    feat: add initial build.gradle.kts configuration for app module with Hilt, Compose, and NDK support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
-            abiFilters.addAll(listOf("arm64-v8a"))
+            abiFilters.addAll(listOf("x86", "x86_64", "armeabi-v7a", "arm64-v8a"))
         }
     }
 
